b60263fff2d856de7f955493dc74be671d3d035d
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-09-26 15:56:50 +0100

    Removed import all to not fill up the namespace

--- src/amulet_nbt/__init__.py
+++ src/amulet_nbt/__init__.py
@@ -1,12 +1,16 @@
 import traceback
 
 try:
-    from .amulet_cy_nbt import *
+    from .amulet_cy_nbt import TAG_Byte, TAG_Short, TAG_Int, TAG_Long, TAG_Float, TAG_Double, \
+        TAG_Byte_Array, TAG_String, TAG_List, TAG_Compound, TAG_Int_Array, TAG_Long_Array, \
+        NBTFile, load#, from_snbt
 
     if __debug__:
         print("Using Amulet NBT library")
 except (ImportError, ModuleNotFoundError) as e:
-    from .amulet_py_nbt import *
+    from .amulet_py_nbt import TAG_Byte, TAG_Short, TAG_Int, TAG_Long, TAG_Float, TAG_Double, \
+        TAG_Byte_Array, TAG_String, TAG_List, TAG_Compound, TAG_Int_Array, TAG_Long_Array, \
+        NBTFile, load, from_snbt
 
     if __debug__:
         traceback.print_exc()

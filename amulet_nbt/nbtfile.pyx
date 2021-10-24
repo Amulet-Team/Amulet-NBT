bb10cded2d8b39c12b1f624cc256ed5319fc6380
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2021-10-24 16:34:36 +0100

    Fixed backwards compatibility of the NBTFile class
    This is depreciated and will be removed in the future

--- amulet_nbt/nbtfile.pyx
+++ amulet_nbt/nbtfile.pyx
@@ -1,3 +1,4 @@
+import warnings
 from typing import Iterable
 from .value cimport BaseTag
 from .compound cimport TAG_Compound
@@ -30,8 +31,33 @@ cdef class NBTFile:
     def __repr__(self):
         return f'{self.__class__.__name__}({repr(self.value)}, "{self.name}")'
 
+    def __dir__(self) -> Iterable[str]:
+        return list(set(list(super().__dir__()) + dir(self.value)))
+
+    def __reduce__(self):
+        return NBTFile, (self.value, self.name)
+
+    # All of the methods below are depreciated. You should use the .value attribute to get the object.
     def __getattr__(self, item):
+        warnings.warn("__getattr__ is deprecated. Use .value to get the object instead.", DeprecationWarning)
         return getattr(self.value, item)
 
-    def __dir__(self) -> Iterable[str]:
-        return list(set(dir(self.__class__) + dir(self.value)))
+    def __getitem__(self, item):
+        warnings.warn("__getitem__ is deprecated. Use .value to get the object instead.", DeprecationWarning)
+        return self.value[item]
+
+    def __setitem__(self, key, value):
+        warnings.warn("__setitem__ is deprecated. Use .value to get the object instead.", DeprecationWarning)
+        self.value[key] = value
+
+    def __len__(self) -> int:
+        warnings.warn("__len__ is deprecated. Use .value to get the object instead.", DeprecationWarning)
+        return self.value.__len__()
+
+    def __delitem__(self, key: str):
+        warnings.warn("__delitem__ is deprecated. Use .value to get the object instead.", DeprecationWarning)
+        del self.value[key]
+
+    def __contains__(self, key: str) -> bool:
+        warnings.warn("__contains__ is deprecated. Use .value to get the object instead.", DeprecationWarning)
+        return key in self.value

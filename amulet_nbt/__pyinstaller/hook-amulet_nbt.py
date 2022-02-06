dd131ce5095faacdb9f1976b2f7ae6ea80ebb074
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-02-06 09:40:41 +0000

    Reformatted the pyinstaller hook

--- amulet_nbt/__pyinstaller/hook-amulet_nbt.py
+++ amulet_nbt/__pyinstaller/hook-amulet_nbt.py
@@ -2,6 +2,9 @@ import pkgutil
 
 import amulet_nbt
 
-hiddenimports = (
-    [name for _, name, _ in pkgutil.walk_packages(amulet_nbt.__path__, amulet_nbt.__name__ + ".")]
-)
+hiddenimports = [
+    name
+    for _, name, _ in pkgutil.walk_packages(
+        amulet_nbt.__path__, amulet_nbt.__name__ + "."
+    )
+]

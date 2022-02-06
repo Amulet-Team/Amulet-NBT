34a310107f743c4f6434aa87ea88c4e06075e44f
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-02-06 11:39:14 +0000

    Exposed the named tags in the library init

--- amulet_nbt/__init__.pxd
+++ amulet_nbt/__init__.pxd
@@ -1,16 +1,76 @@
+# Base Types
 from .value cimport BaseTag, BaseImmutableTag, BaseMutableTag
 from .numeric cimport BaseNumericTag
-from .int cimport BaseIntTag, TAG_Byte, TAG_Short, TAG_Int, TAG_Long
-from .float cimport BaseFloatTag, TAG_Float, TAG_Double
+
+# Types
+from .int cimport (
+    BaseIntTag,
+    TAG_Byte,
+    TAG_Short,
+    TAG_Int,
+    TAG_Long,
+    Named_TAG_Byte,
+    Named_TAG_Short,
+    Named_TAG_Int,
+    Named_TAG_Long,
+    TAG_Byte as ByteTag,
+    TAG_Short as ShortTag,
+    TAG_Int as IntTag,
+    TAG_Long as LongTag,
+    Named_TAG_Byte as NamedByteTag,
+    Named_TAG_Short as NamedShortTag,
+    Named_TAG_Int as NamedIntTag,
+    Named_TAG_Long as NamedLongTag,
+)
+from .float cimport (
+    BaseFloatTag,
+    TAG_Float,
+    TAG_Double,
+    Named_TAG_Float,
+    Named_TAG_Double,
+    TAG_Float as FloatTag,
+    TAG_Double as DoubleTag,
+    Named_TAG_Float as NamedFloatTag,
+    Named_TAG_Double as NamedDoubleTag,
+)
 from .array cimport (
     BaseArrayTag,
+    BaseArrayType,
     TAG_Byte_Array,
     TAG_Int_Array,
     TAG_Long_Array,
+    Named_TAG_Byte_Array,
+    Named_TAG_Int_Array,
+    Named_TAG_Long_Array,
+    TAG_Byte_Array as ByteArrayTag,
+    TAG_Int_Array as IntArrayTag,
+    TAG_Long_Array as LongArrayTag,
+    Named_TAG_Byte_Array as NamedByteArrayTag,
+    Named_TAG_Int_Array as NamedIntArrayTag,
+    Named_TAG_Long_Array as NamedLongArrayTag,
 )
-from .string cimport TAG_String
-from .list cimport TAG_List
-from .compound cimport TAG_Compound
+from .string cimport (
+    TAG_String,
+    Named_TAG_String,
+    TAG_String as StringTag,
+    Named_TAG_String as NamedStringTag,
+)
+from .list cimport (
+    TAG_List,
+    Named_TAG_List,
+    TAG_List as ListTag,
+    Named_TAG_List as NamedListTag,
+)
+from .compound cimport (
+    TAG_Compound,
+    Named_TAG_Compound,
+    TAG_Compound as CompoundTag,
+    Named_TAG_Compound as NamedCompoundTag,
+)
+
+# NBTFile
 from .nbtfile cimport NBTFile
+
+# Load functions
 from .load_nbt cimport load, load_one, load_many
 from .load_snbt cimport from_snbt
--- amulet_nbt/__init__.py
+++ amulet_nbt/__init__.py
@@ -1,20 +1,80 @@
+# Base Types
 from .value import BaseTag, BaseImmutableTag, BaseMutableTag, BaseValueType
 from .numeric import BaseNumericTag
-from .int import BaseIntTag, TAG_Byte, TAG_Short, TAG_Int, TAG_Long
-from .float import BaseFloatTag, TAG_Float, TAG_Double
+
+# Types
+from .int import (
+    BaseIntTag,
+    TAG_Byte,
+    TAG_Short,
+    TAG_Int,
+    TAG_Long,
+    Named_TAG_Byte,
+    Named_TAG_Short,
+    Named_TAG_Int,
+    Named_TAG_Long,
+    TAG_Byte as ByteTag,
+    TAG_Short as ShortTag,
+    TAG_Int as IntTag,
+    TAG_Long as LongTag,
+    Named_TAG_Byte as NamedByteTag,
+    Named_TAG_Short as NamedShortTag,
+    Named_TAG_Int as NamedIntTag,
+    Named_TAG_Long as NamedLongTag,
+)
+from .float import (
+    BaseFloatTag,
+    TAG_Float,
+    TAG_Double,
+    Named_TAG_Float,
+    Named_TAG_Double,
+    TAG_Float as FloatTag,
+    TAG_Double as DoubleTag,
+    Named_TAG_Float as NamedFloatTag,
+    Named_TAG_Double as NamedDoubleTag,
+)
 from .array import (
     BaseArrayTag,
     BaseArrayType,
     TAG_Byte_Array,
     TAG_Int_Array,
     TAG_Long_Array,
+    Named_TAG_Byte_Array,
+    Named_TAG_Int_Array,
+    Named_TAG_Long_Array,
+    TAG_Byte_Array as ByteArrayTag,
+    TAG_Int_Array as IntArrayTag,
+    TAG_Long_Array as LongArrayTag,
+    Named_TAG_Byte_Array as NamedByteArrayTag,
+    Named_TAG_Int_Array as NamedIntArrayTag,
+    Named_TAG_Long_Array as NamedLongArrayTag,
 )
-from .string import TAG_String
-from .list import TAG_List
-from .compound import TAG_Compound
+from .string import (
+    TAG_String,
+    Named_TAG_String,
+    TAG_String as StringTag,
+    Named_TAG_String as NamedStringTag,
+)
+from .list import (
+    TAG_List,
+    Named_TAG_List,
+    TAG_List as ListTag,
+    Named_TAG_List as NamedListTag,
+)
+from .compound import (
+    TAG_Compound,
+    Named_TAG_Compound,
+    TAG_Compound as CompoundTag,
+    Named_TAG_Compound as NamedCompoundTag,
+)
+
+# NBTFile
 from .nbtfile import NBTFile
+
+# Load functions
 from .load_nbt import load, load_one, load_many
 from .load_snbt import from_snbt
+
 from .errors import NBTError, NBTLoadError, NBTFormatError, SNBTParseError
 
 from .dtype import SNBTType, IntType, FloatType, NumberType, ArrayType, AnyNBT

368aa9a54d17c39a900fa47f0535deb955a8d117
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-02-08 14:28:41 +0000

    Added a NamedTag type

--- amulet_nbt/dtype.pyx
+++ amulet_nbt/dtype.pyx
@@ -1,15 +1,41 @@
 from typing import Union
 
-from .int cimport ByteTag, ShortTag, IntTag, LongTag
-from .float cimport FloatTag, DoubleTag
+from .int cimport (
+    ByteTag,
+    ShortTag,
+    IntTag,
+    LongTag,
+    NamedByteTag,
+    NamedShortTag,
+    NamedIntTag,
+    NamedLongTag,
+)
+from .float cimport (
+    FloatTag,
+    DoubleTag,
+    NamedFloatTag,
+    NamedDoubleTag,
+)
 from .array cimport (
     ByteArrayTag,
     IntArrayTag,
     LongArrayTag,
+    NamedByteArrayTag,
+    NamedIntArrayTag,
+    NamedLongArrayTag,
 )
-from .string cimport StringTag
-from .list cimport ListTag
-from .compound cimport CompoundTag
+from .string cimport (
+    StringTag,
+    NamedStringTag,
+)
+from .list cimport (
+    ListTag,
+    NamedListTag,
+)
+from .compound cimport (
+    CompoundTag,
+    NamedCompoundTag,
+)
 
 SNBTType = str
 
@@ -53,4 +79,19 @@ AnyNBT = Union[
     CompoundTag,
     IntArrayTag,
     LongArrayTag
+]
+
+NamedTag = Union[
+    NamedByteTag,
+    NamedShortTag,
+    NamedIntTag,
+    NamedLongTag,
+    NamedFloatTag,
+    NamedDoubleTag,
+    NamedByteArrayTag,
+    NamedStringTag,
+    NamedListTag,
+    NamedCompoundTag,
+    NamedIntArrayTag,
+    NamedLongArrayTag,
 ]

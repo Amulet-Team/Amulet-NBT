02d082f5f4d203de6dc6170e7e342c7ab0710ba3
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-06-22 12:10:05 +0100

    Added more interface files

---
+++ amulet_nbt/_load_nbt.pyi
@@ -0,0 +1,25 @@
+from typing import Union, BinaryIO, List
+from ._util import DecoderType
+from mutf8 import decode_modified_utf8
+from ._named_tag import NamedTag
+
+class ReadContext:
+    offset: int
+
+def load_one(
+    filepath_or_buffer: Union[str, bytes, BinaryIO, None],
+    *,
+    compressed: bool = True,
+    little_endian: bool = False,
+    read_context: ReadContext = None,
+    string_decoder: DecoderType = decode_modified_utf8
+) -> NamedTag: ...
+def load_many(
+    filepath_or_buffer: Union[str, bytes, BinaryIO, None],
+    *,
+    count: int = 1,
+    compressed: bool = True,
+    little_endian: bool = False,
+    read_context: ReadContext = None,
+    string_decoder: DecoderType = decode_modified_utf8
+) -> List[NamedTag]: ...

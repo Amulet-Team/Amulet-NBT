cee89bb94581e47b31b97b63b2b7ee2a26f5a2f0
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-07-14 15:33:49 +0100

    Added mutf8 as a hidden import

--- amulet_nbt/__pyinstaller/hook-amulet_nbt.py
+++ amulet_nbt/__pyinstaller/hook-amulet_nbt.py
@@ -2,7 +2,7 @@ import pkgutil
 
 import amulet_nbt
 
-hiddenimports = [
+hiddenimports = ["mutf8"] + [
     name
     for _, name, _ in pkgutil.walk_packages(
         amulet_nbt.__path__, amulet_nbt.__name__ + "."

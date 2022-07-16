55a61106f0926521e8c6cfc384ff91c07df054fe
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-07-16 14:22:27 +0100

    Fixed versioneer

--- src/amulet_nbt/_version.py
+++ src/amulet_nbt/_version.py
@@ -44,7 +44,7 @@ def get_config():
     cfg.style = "pep440"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = "amulet_nbt-"
-    cfg.versionfile_source = "amulet_nbt/_version.py"
+    cfg.versionfile_source = "src/amulet_nbt/_version.py"
     cfg.verbose = False
     return cfg
 

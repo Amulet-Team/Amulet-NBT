08cd6c649ad2deb764b8fc89bfb46649f8626366
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-06-28 14:39:09 +0100

    Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,9 @@ from Cython.Build import cythonize
 import versioneer
 import numpy
 
-# Note this will error if no pyx files are present
-ext = cythonize(
-    f"src/**/*.pyx",
-    language_level=3,
-)
-
 setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     include_dirs=[numpy.get_include()],
-    ext_modules=ext,
+    ext_modules=cythonize(f"src/**/*.pyx", language_level=3),
 )

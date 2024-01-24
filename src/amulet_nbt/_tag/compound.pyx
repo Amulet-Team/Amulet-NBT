6cb4af786b3ce7aa7fb51d8ea7a57db7aaf46467
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-01-24 14:46:59 +0000

    Added compound tag methods

--- src/amulet_nbt/_tag/compound.pyx
+++ src/amulet_nbt/_tag/compound.pyx
@@ -6,7 +6,7 @@
 # distutils: extra_link_args = -std=c++20 /std:c++20
 # cython: c_string_type=str, c_string_encoding=utf8
 
-from collections.abc import MutableMapping, Iterable
+from collections.abc import Mapping, Iterable, KeysView, ItemsView, ValuesView
 
 from libcpp.string cimport string
 from libcpp cimport bool
@@ -15,7 +15,6 @@ from cython.operator cimport dereference, postincrement
 from amulet_nbt._nbt cimport CCompoundTag, CCompoundTagPtr, CIntTag, TagNode
 from amulet_nbt._libcpp.variant cimport get
 from amulet_nbt._tag.abc cimport AbstractBaseTag, AbstractBaseMutableTag
-from .int cimport IntTag
 
 from amulet_nbt._nbt cimport (
     CByteTag,
@@ -49,6 +48,13 @@ from amulet_nbt._nbt cimport (
     CLongArrayList,
     TagNode,
 )
+
+from .int cimport ByteTag, ShortTag, IntTag, LongTag
+from .float cimport FloatTag, DoubleTag
+from .string cimport StringTag
+from .list cimport is_list_eq, ListTag
+from .array cimport ByteArrayTag, IntArrayTag, LongArrayTag
+
 
 cdef inline bool _is_byte_tag_node_eq(TagNode* a, TagNode* b) noexcept nogil:
     if dereference(b).index() != 1:
@@ -179,10 +185,40 @@ cdef bool is_compound_eq(CCompoundTagPtr a, CCompoundTagPtr b) noexcept nogil:
     return True
 
 
+cdef AbstractBaseTag wrap_node(TagNode* node):
+    cdef size_t index = dereference(node).index()
+    if index == 1:
+        return ByteTag.wrap(get[CByteTag](dereference(node)))
+    elif index == 2:
+        return ShortTag.wrap(get[CShortTag](dereference(node)))
+    elif index == 3:
+        return IntTag.wrap(get[CIntTag](dereference(node)))
+    elif index == 4:
+        return LongTag.wrap(get[CLongTag](dereference(node)))
+    elif index == 5:
+        return FloatTag.wrap(get[CFloatTag](dereference(node)))
+    elif index == 6:
+        return DoubleTag.wrap(get[CDoubleTag](dereference(node)))
+    elif index == 7:
+        return ByteArrayTag.wrap(get[CByteArrayTagPtr](dereference(node)))
+    elif index == 8:
+        return StringTag.wrap(get[CStringTag](dereference(node)))
+    elif index == 9:
+        return ListTag.wrap(get[CListTagPtr](dereference(node)))
+    elif index == 10:
+        return CompoundTag.wrap(get[CCompoundTagPtr](dereference(node)))
+    elif index == 11:
+        return IntArrayTag.wrap(get[CIntArrayTagPtr](dereference(node)))
+    elif index == 12:
+        return LongArrayTag.wrap(get[CLongArrayTagPtr](dereference(node)))
+    else:
+        raise RuntimeError
+
+
 cdef class CompoundTag(AbstractBaseMutableTag):
     """
     A Python wrapper around a C++ unordered map.
-    Note that this class is not thread safe and inherits all the limitations of a C++ map.
+    Note that this class is not thread safe and inherits all the limitations of a C++ unordered_map.
     """
 
     def __init__(self, *args, **kwargs) -> None:
@@ -194,63 +230,17 @@ cdef class CompoundTag(AbstractBaseMutableTag):
         tag.cpp = cpp
         return tag
 
-    def __contains__(self, string key):
-        return dereference(self.cpp).contains(key)
+    def __eq__(CompoundTag self, object other):
+        if not isinstance(other, CompoundTag):
+            return False
+        cdef CompoundTag tag = other
+        return is_compound_eq(self.cpp, tag.cpp)
 
-    def __setitem__(self, string key, AbstractBaseTag value) -> None:
-        cdef TagNode node
-        if isinstance(value, IntTag):
-            node.emplace[CIntTag]((<IntTag> value).cpp)
-        else:
-            raise TypeError
-        dereference(self.cpp)[<string> key] = node
-
-    def __delitem__(self, string key) -> None:
-        dereference(self.cpp).erase(key)
-
-    def __getitem__(self, string key) -> AbstractBaseTag:
-        cdef CCompoundTag.iterator it = dereference(self.cpp).find(key)
-
-        if it == dereference(self.cpp).end():
-            raise KeyError(key)
-
-        cdef TagNode node = dereference(it).second
-        cdef size_t index = node.index()
-        if index == 3:
-            return IntTag(get[CIntTag](node))
-        else:
-            raise RuntimeError
-
-    def get(self, string key, object default = None, object cls = AbstractBaseTag):
-        cdef AbstractBaseTag tag
-        try:
-            tag = self[key]
-        except KeyError as e:
-            if default is None:
-                raise e
-            else:
-                return default
-        else:
-            if isinstance(tag, cls):
-                return tag
-            elif default is None:
-                raise TypeError(f"Expected tag to be of type {cls.__name__} but got {type(tag).__name__}")
-            else:
-                return default
-
-    def setdefault(self, string key, AbstractBaseTag value = None, object cls = AbstractBaseTag):
-        cdef AbstractBaseTag tag
-        try:
-            tag = self[key]
-        except KeyError as e:
-            self[key] = value
-        else:
-            if not isinstance(tag, cls):
-                self[key] = value
-
+    # Sized
     def __len__(self) -> size_t:
         return dereference(self.cpp).size()
 
+    # Iterable
     def __iter__(self) -> Iterable[str | bytes]:
         cdef list keys = []
         cdef CCompoundTag.iterator it = dereference(self.cpp).begin()
@@ -264,5 +254,142 @@ cdef class CompoundTag(AbstractBaseMutableTag):
             postincrement(it)
         return keys
 
+    # Mapping
+    def __getitem__(self, string key) -> AbstractBaseTag:
+        cdef CCompoundTag.iterator it = dereference(self.cpp).find(key)
+
+        if it == dereference(self.cpp).end():
+            raise KeyError(key)
+
+        return wrap_node(&dereference(it).second)
+
+    def get(self, string key, object default = None, object cls = AbstractBaseTag):
+        cdef CCompoundTag.iterator it = dereference(self.cpp).find(key)
+
+        if it == dereference(self.cpp).end():
+            if default is None:
+                raise KeyError(key)
+            else:
+                return default
+
+        cdef AbstractBaseTag tag = wrap_node(&dereference(it).second)
+        if isinstance(tag, cls):
+            return tag
+        elif default is None:
+            raise TypeError(f"Expected tag to be of type {cls.__name__} but got {type(tag).__name__}")
+        else:
+            return default
+
+    def __contains__(self, string key):
+        return dereference(self.cpp).contains(key)
+
+    def keys(self):
+        return KeysView(self)
+
+    def items(self):
+        return ItemsView(self)
+
+    def values(self):
+        return ValuesView(self)
+
+    # MutableMapping
+    def __setitem__(self, string key, AbstractBaseTag value not None) -> None:
+        cdef TagNode node
+        if isinstance(value, ByteTag):
+            node.emplace[CByteTag]((<ByteTag> value).cpp)
+        elif isinstance(value, ShortTag):
+            node.emplace[CShortTag]((<ShortTag> value).cpp)
+        elif isinstance(value, IntTag):
+            node.emplace[CIntTag]((<IntTag> value).cpp)
+        elif isinstance(value, LongTag):
+            node.emplace[CLongTag]((<LongTag> value).cpp)
+        elif isinstance(value, FloatTag):
+            node.emplace[CFloatTag]((<FloatTag> value).cpp)
+        elif isinstance(value, DoubleTag):
+            node.emplace[CDoubleTag]((<DoubleTag> value).cpp)
+        elif isinstance(value, ByteArrayTag):
+            node.emplace[CByteArrayTagPtr]((<ByteArrayTag> value).cpp)
+        elif isinstance(value, StringTag):
+            node.emplace[CStringTag]((<StringTag> value).cpp)
+        elif isinstance(value, ListTag):
+            node.emplace[CListTagPtr]((<ListTag> value).cpp)
+        elif isinstance(value, CompoundTag):
+            node.emplace[CCompoundTagPtr]((<CompoundTag> value).cpp)
+        elif isinstance(value, IntArrayTag):
+            node.emplace[CIntArrayTagPtr]((<IntArrayTag> value).cpp)
+        elif isinstance(value, LongArrayTag):
+            node.emplace[CLongArrayTagPtr]((<LongArrayTag> value).cpp)
+        else:
+            raise TypeError
+        dereference(self.cpp)[<string> key] = node
+
+    def __delitem__(self, string key) -> None:
+        dereference(self.cpp).erase(key)
+
+    __marker = object()
+
+    def pop(self, string key, default=__marker):
+        '''D.pop(k[,d]) -> v, remove specified key and return the corresponding value.
+          If key is not found, d is returned if given, otherwise KeyError is raised.
+        '''
+        cdef CCompoundTag.iterator it = dereference(self.cpp).find(key)
+        if it == dereference(self.cpp).end():
+            if default is self.__marker:
+                raise KeyError(key)
+            else:
+                return default
+
+        cdef AbstractBaseTag tag = wrap_node(&dereference(it).second)
+        dereference(self.cpp).erase(it)
+        return tag
+
+    def popitem(self):
+        '''D.popitem() -> (k, v), remove and return some (key, value) pair
+           as a 2-tuple; but raise KeyError if D is empty.
+        '''
+        cdef CCompoundTag.iterator it = dereference(self.cpp).begin()
+        if it == dereference(self.cpp).end():
+            raise KeyError
+
+        cdef string key = dereference(it).first
+        cdef AbstractBaseTag tag = wrap_node(&dereference(it).second)
+        dereference(self.cpp).erase(it)
+        return key, tag
+
     def clear(self):
         dereference(self.cpp).clear()
+
+    def update(self, other=(), /, **kwds):
+        ''' D.update([E, ]**F) -> None.  Update D from mapping/iterable E and F.
+            If E present and has a .keys() method, does:     for k in E: D[k] = E[k]
+            If E present and lacks .keys() method, does:     for (k, v) in E: D[k] = v
+            In either case, this is followed by: for k, v in F.items(): D[k] = v
+        '''
+        if isinstance(other, Mapping):
+            for key in other:
+                self[key] = other[key]
+        elif hasattr(other, "keys"):
+            for key in other.keys():
+                self[key] = other[key]
+        else:
+            for key, value in other:
+                self[key] = value
+        for key, value in kwds.items():
+            self[key] = value
+
+    def setdefault(self, string key, AbstractBaseTag tag not None, object cls = AbstractBaseTag):
+        cdef CCompoundTag.iterator it = dereference(self.cpp).find(key)
+
+        if it == dereference(self.cpp).end():
+            # if the key does not exist then set it
+            self[key] = tag
+            return tag
+
+        cdef AbstractBaseTag existing_tag = wrap_node(&dereference(it).second)
+
+        if not isinstance(tag, cls):
+            # if the key exists but has the wrong type then set it
+            self[key] = tag
+            return tag
+
+        return existing_tag

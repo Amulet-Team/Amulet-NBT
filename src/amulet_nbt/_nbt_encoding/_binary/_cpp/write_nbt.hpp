21f6e4f99006b8227a934b25256431c7e3f4812a
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-02-18 13:42:46 +0000

    Moved template

--- src/amulet_nbt/_nbt_encoding/_binary/_cpp/write_nbt.hpp
+++ src/amulet_nbt/_nbt_encoding/_binary/_cpp/write_nbt.hpp
@@ -28,6 +28,17 @@ void write_int_array_payload(BinaryWriter& writer, const CIntArrayTagPtr& value)
 void write_long_array_payload(BinaryWriter& writer, const CLongArrayTagPtr& value);
 
 
+template<typename V, typename T, size_t I = 0>
+constexpr size_t variant_index() {
+    static_assert(I < std::variant_size_v<V>, "Type T is not a member of variant V");
+    if constexpr (std::is_same_v<std::variant_alternative_t<I, V>, T>) {
+        return (I);
+    } else {
+        return (variant_index<V, T, I + 1>());
+    }
+}
+
+
 template <
     typename T,
     std::enable_if_t<
@@ -110,17 +121,6 @@ std::string write_named_tag(const std::string& name, const T& tag, std::endian e
     BinaryWriter writer(endianness, stringEncode);
     write_named_tag<T>(writer, name, tag);
     return writer.getBuffer();
-}
-
-
-template<typename V, typename T, size_t I = 0>
-constexpr size_t variant_index() {
-    static_assert(I < std::variant_size_v<V>, "Type T is not a member of variant V");
-    if constexpr (std::is_same_v<std::variant_alternative_t<I, V>, T>) {
-        return (I);
-    } else {
-        return (variant_index<V, T, I + 1>());
-    }
 }
 
 

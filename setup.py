09e5eced098a63c9860237fc235b2099f8649b20
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-06-21 09:19:00 +0100

    Refactor setup.py to work with pybind11 module

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
-from Cython.Build import cythonize
+from setuptools import setup, Extension
 import versioneer
 import numpy
 import sysconfig
 from distutils import ccompiler
 import sys
+import pybind11
+import glob
 
 if (sysconfig.get_config_var("CXX") or ccompiler.get_default_compiler()).split()[
     0
@@ -20,8 +21,16 @@ if sys.platform == "darwin":
 setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    include_dirs=[numpy.get_include()],
-    ext_modules=cythonize(
-        f"src/**/*.pyx", language_level=3, aliases={"CPPCARGS": CompileArgs}
-    ),
+    include_dirs=["src/amulet_nbt/include", numpy.get_include(), pybind11.get_include()],
+    ext_modules=[
+        Extension(
+            name="amulet_nbt._nbt",
+            sources=[
+                *glob.glob("src/amulet_nbt/py/**/*.cpp", recursive=True),
+                # TODO: The following should be moved to a dynamic library.
+                *glob.glob("src/amulet_nbt/cpp/**/*.cpp", recursive=True)
+            ],
+            extra_compile_args=CompileArgs
+        )
+    ]
 )

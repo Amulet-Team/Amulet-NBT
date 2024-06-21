29b73534b4ca64fb14e7975ce64922794196917f
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-06-21 09:28:18 +0100

    Add common header

---
+++ src/amulet_nbt/include/amulet_nbt/common.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#define PYBIND11_DETAILED_ERROR_MESSAGES
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/operators.h>
+#include <pybind11/numpy.h>
+
+namespace py = pybind11;

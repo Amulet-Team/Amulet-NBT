52b1b7cc03dc2a88266f2d25adcad310db34beb4
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-06-28 10:06:01 +0100

    Added list functions

---
+++ src/amulet_nbt/cpp/tag/list.cpp
@@ -0,0 +1,37 @@
+#include <type_traits>
+#include <amulet_nbt/common.hpp>
+#include <amulet_nbt/tag/wrapper.hpp>
+#include <amulet_nbt/tag/list.hpp>
+#include <amulet_nbt/tag/compound.hpp>
+
+namespace Amulet {
+    size_t ListTag_size(const Amulet::ListTag& self){
+        switch(self.index()){
+            #define CASE(ID, TAG_NAME, TAG, TAG_STORAGE, LIST_TAG) case ID: return get<LIST_TAG>(self).size();
+            FOR_EACH_LIST_TAG(CASE)
+            #undef CASE
+        }
+        return 0;
+    };
+
+    void ListTag_append(Amulet::ListTag& self, Amulet::TagNode tag){
+        switch(self.index()){
+            #define CASE(ID, TAG_NAME, TAG, TAG_STORAGE, LIST_TAG)\
+            case ID:\
+                ListTag_append<TAG_STORAGE>(self, get<TAG_STORAGE>(tag));\
+                break;
+            case 0:
+                throw std::invalid_argument("Cannot append null TagNode");
+            FOR_EACH_LIST_TAG(CASE)
+            #undef CASE
+        }
+    }
+
+    ListTagIterator::ListTagIterator(Amulet::ListTagPtr tag, size_t start, std::ptrdiff_t step): tag(tag), index(start), step(step) {};
+    Amulet::TagNode ListTagIterator::next() {
+        return Amulet::ListTag_get_node<size_t>(*tag, index++);
+    }
+    bool ListTagIterator::has_next(){
+        return index >= 0 && index < ListTag_size(*tag);
+    }
+}

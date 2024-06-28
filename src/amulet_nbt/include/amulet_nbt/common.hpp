5a04c5084b8f5e7cd45c8d4cae5e2939b359b030
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-06-28 10:06:34 +0100

    Added a template function to find the variant index

--- src/amulet_nbt/include/amulet_nbt/common.hpp
+++ src/amulet_nbt/include/amulet_nbt/common.hpp
@@ -1,10 +1,14 @@
 #pragma once
 
-#define PYBIND11_DETAILED_ERROR_MESSAGES
+#include <type_traits>
+#include <variant>
 
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
-#include <pybind11/operators.h>
-#include <pybind11/numpy.h>
-
-namespace py = pybind11;
+template<typename V, typename T, size_t I = 0>
+constexpr size_t variant_index() {
+    static_assert(I < std::variant_size_v<V>, "Type T is not a member of variant V");
+    if constexpr (std::is_same_v<std::variant_alternative_t<I, V>, T>) {
+        return (I);
+    } else {
+        return (variant_index<V, T, I + 1>());
+    }
+}

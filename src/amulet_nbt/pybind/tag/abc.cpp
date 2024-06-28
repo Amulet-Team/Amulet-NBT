707f608b23d460bc7227a314ba36718f942705f5
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-06-28 16:40:56 +0100

    Refactor abstract method

--- src/amulet_nbt/pybind/tag/abc.cpp
+++ src/amulet_nbt/pybind/tag/abc.cpp
@@ -7,7 +7,7 @@
 namespace py = pybind11;
 
 template <typename T>
-void abstract_method(const T& self){
+void abstract_method(T self){
     PyErr_SetString(PyExc_NotImplementedError, "");
     throw py::error_already_set();
 }
@@ -19,7 +19,7 @@ void init_abc(py::module& m) {
     );
         AbstractBaseTag.def_property_readonly(
             "py_data",
-            abstract_method<Amulet::AbstractBaseTag>,
+            abstract_method<const Amulet::AbstractBaseTag&>,
             "A python representation of the class. Note that the return type is undefined and may change in the future.\n"\
             "\n"\
             "You would be better off using the py_{type} or np_array properties if you require a fixed type.\n"\
@@ -27,46 +27,46 @@ void init_abc(py::module& m) {
         );
         AbstractBaseTag.def(
             "to_nbt",
-            abstract_method<Amulet::AbstractBaseTag>
+            abstract_method<const Amulet::AbstractBaseTag&>
         );
         AbstractBaseTag.def(
             "save_to",
-            abstract_method<Amulet::AbstractBaseTag>
+            abstract_method<const Amulet::AbstractBaseTag&>
         );
         AbstractBaseTag.def(
             "to_snbt",
-            abstract_method<Amulet::AbstractBaseTag>
+            abstract_method<const Amulet::AbstractBaseTag&>
         );
         AbstractBaseTag.def(
             "__eq__",
-            abstract_method<Amulet::AbstractBaseTag>,
+            abstract_method<const Amulet::AbstractBaseTag&>,
             "Check if the instance is equal to another instance.\n"
             "\n"
             "This will only return True if the tag type is the same and the data contained is the same."
         );
         AbstractBaseTag.def(
             "__repr__",
-            abstract_method<Amulet::AbstractBaseTag>,
+            abstract_method<const Amulet::AbstractBaseTag&>,
             "A string representation of the object to show how it can be constructed."
         );
         AbstractBaseTag.def(
             "__str__",
-            abstract_method<Amulet::AbstractBaseTag>,
+            abstract_method<const Amulet::AbstractBaseTag&>,
             "A string representation of the object."
         );
         AbstractBaseTag.def(
             "__reduce__",
-            abstract_method<Amulet::AbstractBaseTag>,
+            abstract_method<const Amulet::AbstractBaseTag&>,
             "A string representation of the object."
         );
         AbstractBaseTag.def(
             "__copy__",
-            abstract_method<Amulet::AbstractBaseTag>,
+            abstract_method<const Amulet::AbstractBaseTag&>,
             "A string representation of the object."
         );
         AbstractBaseTag.def(
             "__deepcopy__",
-            abstract_method<Amulet::AbstractBaseTag>,
+            abstract_method<const Amulet::AbstractBaseTag&>,
             "A string representation of the object."
         );
 
@@ -75,7 +75,7 @@ void init_abc(py::module& m) {
     );
         AbstractBaseImmutableTag.def(
             "__hash__",
-            abstract_method<Amulet::AbstractBaseImmutableTag>,
+            abstract_method<const Amulet::AbstractBaseImmutableTag&>,
             "A hash of the data in the class."
         );
 
@@ -84,17 +84,17 @@ void init_abc(py::module& m) {
     );
         AbstractBaseNumericTag.def(
             "__int__",
-            abstract_method<Amulet::AbstractBaseNumericTag>,
+            abstract_method<const Amulet::AbstractBaseNumericTag&>,
             "Get a python int representation of the class."
         );
         AbstractBaseNumericTag.def(
             "__float__",
-            abstract_method<Amulet::AbstractBaseNumericTag>,
+            abstract_method<const Amulet::AbstractBaseNumericTag&>,
             "Get a python float representation of the class."
         );
         AbstractBaseNumericTag.def(
             "__bool__",
-            abstract_method<Amulet::AbstractBaseNumericTag>,
+            abstract_method<const Amulet::AbstractBaseNumericTag&>,
             "Get a python bool representation of the class."
         );
 
@@ -103,7 +103,7 @@ void init_abc(py::module& m) {
     );
         AbstractBaseIntTag.def_property_readonly(
             "py_int",
-            abstract_method<Amulet::AbstractBaseTag>,
+            abstract_method<const Amulet::AbstractBaseTag&>,
             "A python int representation of the class.\n"
             "\n"
             "The returned data is immutable so changes will not mirror the instance."
@@ -114,7 +114,7 @@ void init_abc(py::module& m) {
     );
         AbstractBaseFloatTag.def_property_readonly(
             "py_float",
-            abstract_method<Amulet::AbstractBaseTag>,
+            abstract_method<const Amulet::AbstractBaseTag&>,
             "A python float representation of the class.\n"
             "\n"
             "The returned data is immutable so changes will not mirror the instance."

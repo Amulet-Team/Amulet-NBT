74cc227a93126ea64ae7632953e013b6e1af9c10
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-06-28 10:01:05 +0100

    Move TagWrapper base class out of template
    The template now only takes the tag and the base class is deduced in the class.
    This allows looking up the template based only on the tag.

--- src/amulet_nbt/include/amulet_nbt/tag/wrapper.hpp
+++ src/amulet_nbt/include/amulet_nbt/tag/wrapper.hpp
@@ -16,22 +16,37 @@ namespace Amulet {
 
     // pybind cannot directly store fundamental types
     // This wrapper exists to allow pybind to store fundamental types
-    template <typename BaseClassT, typename T>
-    class TagWrapper: public BaseClassT {
+    template <typename T>
+    class TagWrapper: public
+        std::conditional_t<std::is_same_v<T, ByteTag>, AbstractBaseIntTag,
+        std::conditional_t<std::is_same_v<T, ShortTag>, AbstractBaseIntTag,
+        std::conditional_t<std::is_same_v<T, IntTag>, AbstractBaseIntTag,
+        std::conditional_t<std::is_same_v<T, LongTag>, AbstractBaseIntTag,
+        std::conditional_t<std::is_same_v<T, FloatTag>, AbstractBaseFloatTag,
+        std::conditional_t<std::is_same_v<T, DoubleTag>, AbstractBaseFloatTag,
+        std::conditional_t<std::is_same_v<T, ByteArrayTagPtr>, AbstractBaseArrayTag,
+        std::conditional_t<std::is_same_v<T, StringTag>, AbstractBaseImmutableTag,
+        std::conditional_t<std::is_same_v<T, ListTagPtr>, AbstractBaseMutableTag,
+        std::conditional_t<std::is_same_v<T, CompoundTagPtr>, AbstractBaseMutableTag,
+        std::conditional_t<std::is_same_v<T, IntArrayTagPtr>, AbstractBaseArrayTag,
+        std::conditional_t<std::is_same_v<T, LongArrayTagPtr>, AbstractBaseArrayTag,
+        void
+    >>>>>>>>>>>> {
         public:
             T tag;
             TagWrapper(T tag): tag(tag) {};
     };
-    typedef TagWrapper<AbstractBaseIntTag, ByteTag> ByteTagWrapper;
-    typedef TagWrapper<AbstractBaseIntTag, ShortTag> ShortTagWrapper;
-    typedef TagWrapper<AbstractBaseIntTag, IntTag> IntTagWrapper;
-    typedef TagWrapper<AbstractBaseIntTag, LongTag> LongTagWrapper;
-    typedef TagWrapper<AbstractBaseFloatTag, FloatTag> FloatTagWrapper;
-    typedef TagWrapper<AbstractBaseFloatTag, DoubleTag> DoubleTagWrapper;
-    typedef TagWrapper<AbstractBaseArrayTag, ByteArrayTagPtr> ByteArrayTagWrapper;
-    typedef TagWrapper<AbstractBaseImmutableTag, StringTag> StringTagWrapper;
-    typedef TagWrapper<AbstractBaseMutableTag, ListTagPtr> ListTagWrapper;
-    typedef TagWrapper<AbstractBaseMutableTag, CompoundTagPtr> CompoundTagWrapper;
-    typedef TagWrapper<AbstractBaseArrayTag, IntArrayTagPtr> IntArrayTagWrapper;
-    typedef TagWrapper<AbstractBaseArrayTag, LongArrayTagPtr> LongArrayTagWrapper;
+    typedef TagWrapper<ByteTag> ByteTagWrapper;
+    typedef TagWrapper<ShortTag> ShortTagWrapper;
+    typedef TagWrapper<IntTag> IntTagWrapper;
+    typedef TagWrapper<LongTag> LongTagWrapper;
+    typedef TagWrapper<FloatTag> FloatTagWrapper;
+    typedef TagWrapper<DoubleTag> DoubleTagWrapper;
+    typedef TagWrapper<ByteArrayTagPtr> ByteArrayTagWrapper;
+    typedef TagWrapper<StringTag> StringTagWrapper;
+    typedef TagWrapper<ListTagPtr> ListTagWrapper;
+    typedef TagWrapper<CompoundTagPtr> CompoundTagWrapper;
+    typedef TagWrapper<IntArrayTagPtr> IntArrayTagWrapper;
+    typedef TagWrapper<LongArrayTagPtr> LongArrayTagWrapper;
+
 }

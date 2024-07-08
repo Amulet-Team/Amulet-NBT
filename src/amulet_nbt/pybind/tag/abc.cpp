3240bb0118fc914cfd4806c8f04ef43c871f9d40
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-07-08 14:10:55 +0100

    Implemented save_to

--- src/amulet_nbt/pybind/tag/abc.cpp
+++ src/amulet_nbt/pybind/tag/abc.cpp
@@ -1,3 +1,5 @@
+#include <fstream>
+
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/operators.h>
@@ -89,9 +91,75 @@ void init_abc(py::module& m) {
             py::arg("string_encoding") = mutf8_encoding,
             py::arg("name") = ""
         );
+        auto save_to = [to_nbt](
+            const Amulet::AbstractBaseTag& self,
+            py::object filepath_or_writable,
+            std::string name,
+            bool compressed,
+            std::endian endianness,
+            Amulet::StringEncode string_encoder
+        ){
+            py::bytes py_data = to_nbt(self, name, compressed, endianness, string_encoder);
+            if (!filepath_or_writable.is(py::none())){
+                if (py::isinstance<py::str>(filepath_or_writable)){
+                    std::string data = py_data.cast<std::string>();
+                    std::ofstream file(filepath_or_writable.cast<std::string>(), std::ios::out | std::ios::binary | std::ios::trunc);
+                    file.write(data.c_str(), data.size());
+                } else {
+                    filepath_or_writable.attr("write")(py_data);
+                }
+            }
+            return py_data;
+        };
         AbstractBaseTag.def(
             "save_to",
-            abstract_method<const Amulet::AbstractBaseTag&>
+            [save_to](
+                const Amulet::AbstractBaseTag& self,
+                py::object filepath_or_writable,
+                Amulet::EncodingPreset preset,
+                std::string name
+            ){
+                return save_to(
+                    self,
+                    filepath_or_writable,
+                    name,
+                    preset.compressed,
+                    preset.endianness,
+                    preset.string_encoding.encode
+                );
+            },
+            py::arg("filepath_or_writable"),
+            py::pos_only(),
+            py::kw_only(),
+            py::arg("preset") = java_encoding,
+            py::arg("name") = ""
+        );
+        AbstractBaseTag.def(
+            "save_to",
+            [save_to](
+                const Amulet::AbstractBaseTag& self,
+                py::object filepath_or_writable,
+                bool compressed,
+                bool little_endian,
+                Amulet::StringEncoding string_encoding,
+                std::string name
+            ){
+                return save_to(
+                    self,
+                    filepath_or_writable,
+                    name,
+                    compressed,
+                    little_endian ? std::endian::little : std::endian::big,
+                    string_encoding.encode
+                );
+            },
+            py::arg("filepath_or_writable"),
+            py::pos_only(),
+            py::kw_only(),
+            py::arg("compressed") = true,
+            py::arg("little_endian") = false,
+            py::arg("string_encoding") = mutf8_encoding,
+            py::arg("name") = ""
         );
         AbstractBaseTag.def(
             "to_snbt",

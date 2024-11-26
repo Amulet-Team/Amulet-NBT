f046a44d0d25abc76f82e3519c9034c35d9f5827
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-11-26 15:22:22 +0000

    Refactored setup.py to call cmake

--- setup.py
+++ setup.py
@@ -1,43 +1,60 @@
-from setuptools import setup, Extension
-import versioneer
-import sysconfig
-from distutils import ccompiler
+import os
+import subprocess
 import sys
+from pathlib import Path
 import pybind11
-import glob
 
-if (sysconfig.get_config_var("CXX") or ccompiler.get_default_compiler()).split()[
-    0
-] == "msvc":
-    CompileArgs = ["/std:c++20"]
-else:
-    CompileArgs = ["-std=c++20"]
+from setuptools import setup, Extension, Command
+from setuptools.command.build_ext import build_ext
 
-if sys.platform == "darwin":
-    CompileArgs.append("-mmacosx-version-min=10.13")
+import versioneer
+
+
+# https://github.com/pybind/cmake_example/blob/master/setup.py
+class CMakeExtension(Extension):
+    def __init__(self, name: str, sourcedir: str = "") -> None:
+        super().__init__(name, sources=[])
+        self.sourcedir = os.fspath(Path(sourcedir).resolve())
+
+
+cmdclass: dict[str, type[Command]] = versioneer.get_cmdclass()
+
+
+class CMakeBuild(cmdclass.get("build_ext", build_ext)):
+    def build_extension(self, ext):
+        ext_fullpath = Path.cwd() / self.get_ext_fullpath("")
+        src_dir = ext_fullpath.parent.resolve()
+
+        platform_args = []
+        if sys.platform == "win32":
+            platform_args.extend(["-G", "Visual Studio 17 2022"])
+            if sys.maxsize > 2**32:
+                platform_args.extend(["-A", "x64"])
+            else:
+                platform_args.extend(["-A", "Win32"])
+            platform_args.extend(["-T", "v143"])
+
+        subprocess.run(
+            [
+                "cmake",
+                *platform_args,
+                f"-DPYTHON_EXECUTABLE={sys.executable}",
+                f"-Dpybind11_DIR={pybind11.get_cmake_dir().replace(os.sep, '/')}",
+                f"-DCMAKE_INSTALL_PREFIX=install",
+                f"-DSRC_INSTALL_DIR={src_dir}",
+                "-B",
+                "build",
+            ]
+        )
+        subprocess.run(["cmake", "--build", "build", "--config", "Release"])
+        subprocess.run(["cmake", "--install", "build", "--config", "Release"])
+
+
+cmdclass["build_ext"] = CMakeBuild
 
 
 setup(
     version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    libraries=[
-        (
-            "amulet_nbt",
-            dict(
-                sources=glob.glob("src/amulet_nbt/cpp/**/*.cpp", recursive=True),
-                include_dirs=["src/amulet_nbt/include"],
-                cflags=CompileArgs,
-            ),
-        )
-    ],
-    ext_modules=[
-        Extension(
-            name="amulet_nbt.__init__",
-            sources=glob.glob("src/amulet_nbt/pybind/**/*.cpp", recursive=True),
-            include_dirs=["src/amulet_nbt/include", pybind11.get_include()],
-            libraries=["amulet_nbt"],
-            define_macros=[("PYBIND11_DETAILED_ERROR_MESSAGES", None)],
-            extra_compile_args=CompileArgs,
-        )
-    ],
+    cmdclass=cmdclass,
+    ext_modules=[CMakeExtension("amulet_nbt.__init__")],
 )

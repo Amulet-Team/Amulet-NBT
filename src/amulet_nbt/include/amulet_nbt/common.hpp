aa829c6102c5ab0cc146e87e7e4b06b49d81a24b
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-02-05 14:39:24 +0000

    Added missing include

--- src/amulet_nbt/include/amulet_nbt/common.hpp
+++ src/amulet_nbt/include/amulet_nbt/common.hpp
@@ -6,6 +6,8 @@
 #include <memory>
 #include <stdexcept>
 
+#include <amulet_nbt/export.hpp>
+
 template<typename V, typename T, size_t I = 0>
 constexpr size_t variant_index() {
     static_assert(I < std::variant_size_v<V>, "Type T is not a member of variant V");

d533d864c83a171cf095633067046a41d237893c
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-02-06 13:39:09 +0000

    Improved compression functions (#102)
    * Improved compression functions
    
    The previous implementation would crash if given more than 4GB.
    The new implementation should handle any size.
    
    * Split tests

--- tests/test_amulet_nbt/test_zlib.py
+++ tests/test_amulet_nbt/test_zlib.py
@@ -11,18 +11,22 @@ from tests.test_amulet_nbt.test_zlib_ import (
 
 
 class ZlibTest(TestCase):
-    def test_zlib(self) -> None:
+    def test_zlib_compress(self) -> None:
         src = os.urandom(10_000_000)
-        cpp_compressed = compress_zlib(src)
-        py_decompressed = zlib.decompress(cpp_compressed)
-        self.assertEqual(src, py_decompressed)
-        cpp_decompressed = decompress_zlib_gzip(cpp_compressed)
-        self.assertEqual(src, cpp_decompressed)
+        compressed = compress_zlib(src)
+        self.assertEqual(src, zlib.decompress(compressed))
 
-    def test_gzip(self) -> None:
+    def test_zlib_decompress(self) -> None:
         src = os.urandom(10_000_000)
-        cpp_compressed = compress_gzip(src)
-        py_decompressed = gzip.decompress(cpp_compressed)
-        self.assertEqual(src, py_decompressed)
-        cpp_decompressed = decompress_zlib_gzip(cpp_compressed)
-        self.assertEqual(src, cpp_decompressed)
+        compressed = zlib.compress(src)
+        self.assertEqual(src, decompress_zlib_gzip(compressed))
+
+    def test_gzip_compress(self) -> None:
+        src = os.urandom(10_000_000)
+        compressed = compress_gzip(src)
+        self.assertEqual(src, gzip.decompress(compressed))
+
+    def test_gzip_decompress(self) -> None:
+        src = os.urandom(10_000_000)
+        compressed = gzip.compress(src)
+        self.assertEqual(src, decompress_zlib_gzip(compressed))

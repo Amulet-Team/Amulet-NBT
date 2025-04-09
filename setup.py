3a20517f9d5a25cbc83ba8e2a7377cdd97f9e0b0
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-04-09 14:09:57 +0100

    Improve build tooling (#108)
    * Implement conditional compiler freezing
    
    If "AMULET_FREEZE_COMPILER" config variable is defined as a true value, the runtime requirement of amulet-compiler-version will be frozen and the compiler name and version will be added to the wheel file name as a build tag.
    amulet-compiler-version must be pre-built and uploaded to pypi before this can be used.
    
    * Update workflows
    
    * Remove git dependency
    
    pypi does not allow git dependencies
    
    * Build specialised version of compiler version
    
    * Update compiler version
    
    * Create sdist and wheel separately

--- setup.py
+++ setup.py
@@ -2,26 +2,39 @@ import os
 import subprocess
 import sys
 from pathlib import Path
-import pybind11
 
 from setuptools import setup, Extension, Command
 from setuptools.command.build_ext import build_ext
 
 import versioneer
 
+dependencies = [
+    "amulet-compiler-target==1.0",
+    "numpy>=1.17,<3.0",
+]
+setup_args = {}
 
-# https://github.com/pybind/cmake_example/blob/master/setup.py
-class CMakeExtension(Extension):
-    def __init__(self, name: str, sourcedir: str = "") -> None:
-        super().__init__(name, sources=[])
-        self.sourcedir = os.fspath(Path(sourcedir).resolve())
-
+try:
+    import amulet_compiler_version
+except ImportError:
+    dependencies.append("amulet-compiler-version==1.3.0")
+else:
+    dependencies.append(
+        f"amulet-compiler-version=={amulet_compiler_version.__version__}"
+    )
+    setup_args["options"] = {
+        "bdist_wheel": {
+            "build_number": f"1.{amulet_compiler_version.compiler_id}.{amulet_compiler_version.compiler_version}"
+        }
+    }
 
 cmdclass: dict[str, type[Command]] = versioneer.get_cmdclass()
 
 
 class CMakeBuild(cmdclass.get("build_ext", build_ext)):
     def build_extension(self, ext):
+        import pybind11
+
         ext_fullpath = Path.cwd() / self.get_ext_fullpath("")
         src_dir = ext_fullpath.parent.resolve()
 
@@ -63,5 +76,7 @@ cmdclass["build_ext"] = CMakeBuild
 setup(
     version=versioneer.get_version(),
     cmdclass=cmdclass,
-    ext_modules=[CMakeExtension("amulet_nbt._amulet_nbt")],
+    ext_modules=[Extension("amulet_nbt._amulet_nbt", [])],
+    install_requires=dependencies,
+    **setup_args,
 )

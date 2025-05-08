ef65573c941fff060fec36a7788722a07fbec9df
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-05-08 09:56:36 +0100

    Reformat

--- tests/test_amulet_nbt/test_binary_nbt.py
+++ tests/test_amulet_nbt/test_binary_nbt.py
@@ -8,8 +8,11 @@ class TestBinaryNBT(TestCase):
     def test_binary_nbt(self) -> None:
         from test_amulet_nbt.test_binary_nbt_ import encode_binary_nbt
 
-        self.assertEqual(b"\x03\x00\x02hi\x00\x00\x00\x05", encode_binary_nbt(NamedTag(IntTag(5), "hi")))
+        self.assertEqual(
+            b"\x03\x00\x02hi\x00\x00\x00\x05",
+            encode_binary_nbt(NamedTag(IntTag(5), "hi")),
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()

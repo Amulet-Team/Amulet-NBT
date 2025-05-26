f4e4c44251d48d172aafae907a2035e0ed26c25e
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-05-26 13:32:23 +0100

    Move requirements into requirements.py

--- build_requires.py
+++ build_requires.py
@@ -4,34 +4,23 @@ import os
 from setuptools import build_meta
 from setuptools.build_meta import *
 
-
-_extension_requirements = [
-    "pybind11[global]==2.13.6",
-    "amulet_io~=1.0",
-    "amulet_pybind11_extensions~=1.0",
-]
+import requirements
 
 
 def get_requires_for_build_wheel(
     config_settings: Union[Mapping[str, Union[str, list[str], None]], None] = None,
 ) -> list[str]:
-    requirements = []
-    requirements.extend(build_meta.get_requires_for_build_wheel(config_settings))
-    requirements.append("wheel")
-    requirements.extend(_extension_requirements)
-    if (
-        config_settings and config_settings.get("AMULET_FREEZE_COMPILER")
-    ) or os.environ.get("AMULET_FREEZE_COMPILER", None):
-        requirements.append(
-            "amulet-compiler-version@git+https://github.com/Amulet-Team/Amulet-Compiler-Version.git@1.0"
-        )
-    return requirements
+    return [
+        *build_meta.get_requires_for_build_wheel(config_settings),
+        "wheel",
+        *requirements.get_build_dependencies(),
+    ]
 
 
 def get_requires_for_build_editable(
     config_settings: Union[Mapping[str, Union[str, list[str], None]], None] = None,
 ) -> list[str]:
-    return (
-        build_meta.get_requires_for_build_editable(config_settings)
-        + _extension_requirements
-    )
+    return [
+        *build_meta.get_requires_for_build_editable(config_settings),
+        *requirements.get_build_dependencies(),
+    ]
--- setup.py
+++ setup.py
@@ -9,55 +9,13 @@ from setuptools.command.build_ext import build_ext
 from packaging.version import Version
 import versioneer
 
+import requirements
 
 def fix_path(path: str) -> str:
     return os.path.realpath(path).replace(os.sep, "/")
 
 
-dependencies = [
-    "amulet-compiler-target==1.0",
-    "numpy>=1.17,<3.0",
-]
-setup_args = {}
-
-
-def add_cpp_dependency(lib_name: str, version_str: str) -> None:
-    version = Version(version_str)
-    if version.is_prerelease:
-        # Breaking API changes can be made between pre-release versions. Pin to this exact release.
-        dependencies.append(f"{lib_name}=={version_str}")
-    else:
-        # Major - breaking API change. Dependents must be updated and recompiled.
-        major = version.major
-        # Minor - backwards compatible API change. Dependents must be recompiled.
-        minor = version.minor
-        # Patch - API unchanged. Dependents must be recompiled.
-        patch = version.micro
-        # Fix - API unchanged. Dependents do not need to be recompiled.
-        dependencies.append(f"{lib_name}~={major}.{minor}.{patch}.0")
-
-
-try:
-    import amulet_compiler_version
-except ImportError:
-    dependencies.append("amulet-compiler-version==1.3.0")
-else:
-    dependencies.append(
-        f"amulet-compiler-version=={amulet_compiler_version.__version__}"
-    )
-    setup_args["options"] = {
-        "bdist_wheel": {
-            "build_number": f"1.{amulet_compiler_version.compiler_id}.{amulet_compiler_version.compiler_version}"
-        }
-    }
-
-try:
-    import amulet.io
-except ImportError:
-    dependencies.append("amulet_io~=1.0")
-else:
-    add_cpp_dependency("amulet_io", amulet.io.__version__)
-
+dependencies = requirements.get_runtime_dependencies()
 
 cmdclass: dict[str, type[Command]] = versioneer.get_cmdclass()
 
@@ -120,5 +78,4 @@ setup(
     cmdclass=cmdclass,
     ext_modules=[Extension("amulet.nbt._amulet_nbt", [])],
     install_requires=dependencies,
-    **setup_args,
 )

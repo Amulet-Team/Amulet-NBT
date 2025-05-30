c55c915ef815f327b91599281be2f8011db84f9a
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-05-30 16:05:24 +0100

    Simplify compatibility check

--- tests/test_amulet_nbt/_test_amulet_nbt.py.cpp
+++ tests/test_amulet_nbt/_test_amulet_nbt.py.cpp
@@ -9,10 +9,8 @@ namespace pyext = Amulet::pybind11_extensions;
 void init_binary_nbt(py::module);
 
 void init_module(py::module m){
-    auto amulet_nbt = py::module::import("amulet.nbt");
-
     pyext::init_compiler_config(m);
-    pyext::check_compatibility(amulet_nbt, m);
+    pyext::check_compatibility(py::module::import("amulet.nbt"), m);
 
     init_binary_nbt(m);
 }

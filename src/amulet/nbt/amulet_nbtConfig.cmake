daf1cd500b9c430264a534dac7422f48899e303b
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-06-09 16:32:11 +0100

    Fix cmake config

--- src/amulet/nbt/amulet_nbtConfig.cmake
+++ src/amulet/nbt/amulet_nbtConfig.cmake
@@ -7,10 +7,13 @@ if (NOT TARGET amulet_nbt)
     find_library(amulet_nbt_LIBRARY NAMES amulet_nbt PATHS "${CMAKE_CURRENT_LIST_DIR}")
     message(STATUS "amulet_nbt_LIBRARY: ${amulet_nbt_LIBRARY}")
 
-    add_library(amulet_nbt SHARED IMPORTED)
-    set_target_properties(amulet_nbt PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES "${amulet_nbt_INCLUDE_DIR}"
-        INTERFACE_LINK_LIBRARIES amulet_io
+    add_library(amulet_nbt_bin SHARED IMPORTED)
+    set_target_properties(amulet_nbt_bin PROPERTIES
         IMPORTED_IMPLIB "${amulet_nbt_LIBRARY}"
     )
+
+    add_library(amulet_nbt INTERFACE)
+    target_link_libraries(amulet_nbt INTERFACE amulet_io)
+    target_link_libraries(amulet_nbt INTERFACE amulet_nbt_bin)
+    target_include_directories(amulet_nbt INTERFACE ${amulet_nbt_INCLUDE_DIR})
 endif()

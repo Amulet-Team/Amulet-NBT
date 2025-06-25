040d6156b75a7b6806e876608d7dc6619b2586c2
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-06-25 15:34:18 +0100

    Move build directory to temp

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import sys
 from pathlib import Path
 import platform
 import datetime
+from tempfile import TemporaryDirectory
 
 from setuptools import setup, Extension, Command
 from setuptools.command.build_ext import build_ext
@@ -57,31 +58,32 @@ class CMakeBuild(cmdclass.get("build_ext", build_ext)):
 
         if subprocess.run(["cmake", "--version"]).returncode:
             raise RuntimeError("Could not find cmake")
-        if subprocess.run(
-            [
-                "cmake",
-                *platform_args,
-                f"-DPYTHON_EXECUTABLE={sys.executable}",
-                f"-Dpybind11_DIR={fix_path(pybind11.get_cmake_dir())}",
-                f"-Damulet_pybind11_extensions_DIR={fix_path(amulet.pybind11_extensions.__path__[0])}",
-                f"-Damulet_io_DIR={fix_path(amulet.io.__path__[0])}",
-                f"-Damulet_zlib_DIR={fix_path(amulet.zlib.__path__[0])}",
-                f"-Damulet_nbt_DIR={fix_path(nbt_src_dir)}",
-                f"-DAMULET_NBT_EXT_DIR={fix_path(ext_dir)}",
-                f"-DCMAKE_INSTALL_PREFIX=install",
-                "-B",
-                "build",
-            ]
-        ).returncode:
-            raise RuntimeError("Error configuring amulet-nbt")
-        if subprocess.run(
-            ["cmake", "--build", "build", "--config", "Release"]
-        ).returncode:
-            raise RuntimeError("Error installing amulet-nbt")
-        if subprocess.run(
-            ["cmake", "--install", "build", "--config", "Release"]
-        ).returncode:
-            raise RuntimeError("Error installing amulet-nbt")
+        with TemporaryDirectory() as tempdir:
+            if subprocess.run(
+                [
+                    "cmake",
+                    *platform_args,
+                    f"-DPYTHON_EXECUTABLE={sys.executable}",
+                    f"-Dpybind11_DIR={fix_path(pybind11.get_cmake_dir())}",
+                    f"-Damulet_pybind11_extensions_DIR={fix_path(amulet.pybind11_extensions.__path__[0])}",
+                    f"-Damulet_io_DIR={fix_path(amulet.io.__path__[0])}",
+                    f"-Damulet_zlib_DIR={fix_path(amulet.zlib.__path__[0])}",
+                    f"-Damulet_nbt_DIR={fix_path(nbt_src_dir)}",
+                    f"-DAMULET_NBT_EXT_DIR={fix_path(ext_dir)}",
+                    f"-DCMAKE_INSTALL_PREFIX=install",
+                    "-B",
+                    tempdir,
+                ]
+            ).returncode:
+                raise RuntimeError("Error configuring amulet-nbt")
+            if subprocess.run(
+                ["cmake", "--build", tempdir, "--config", "Release"]
+            ).returncode:
+                raise RuntimeError("Error building amulet-nbt")
+            if subprocess.run(
+                ["cmake", "--install", tempdir, "--config", "Release"]
+            ).returncode:
+                raise RuntimeError("Error installing amulet-nbt")
 
 
 cmdclass["build_ext"] = CMakeBuild

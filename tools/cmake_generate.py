a6da7fca26e3a7c524bb7681cad8c477a9d5cd18
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-09-01 09:09:03 +0100

    Improve build tools (#138)
    * Improve build tools
    
    * Sleep after pushing to pypi
    
    * Bump zlib requirement
    
    * Bump nbt requirement
    
    * Increase sleep time
    
    * Bump zlib version
    
    * Bump zlib version
    
    * Update to compiler version 4

--- tools/cmake_generate.py
+++ tools/cmake_generate.py
@@ -42,7 +42,7 @@ def main():
             f"-Damulet_zlib_DIR={fix_path(amulet.zlib.__path__[0])}",
             f"-Damulet_nbt_DIR={fix_path(os.path.join(RootDir, 'src', 'amulet', 'nbt'))}",
             f"-DCMAKE_INSTALL_PREFIX=install",
-            f"-DBUILD_AMULET_NBT_TESTS=",
+            f"-DBUILD_AMULET_NBT_TESTS=ON",
             "-B",
             "build",
         ]

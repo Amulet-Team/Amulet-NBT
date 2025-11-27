c82e178060336494f54d046be5bf819685e5f958
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-11-27 15:56:08 +0000

    Update IO library (#146)

--- src/amulet/nbt/nbt_encoding/binary.hpp
+++ src/amulet/nbt/nbt_encoding/binary.hpp
@@ -27,20 +27,20 @@ namespace NBT {
     AMULET_NBT_EXPORT std::vector<NamedTag> decode_nbt_array(std::string_view, std::endian, Amulet::StringDecoder, size_t& offset, bool named = true);
     AMULET_NBT_EXPORT std::vector<NamedTag> decode_nbt_array(std::string_view, std::endian, Amulet::StringDecoder, size_t& offset, size_t count, bool named);
 
-    AMULET_NBT_EXPORT void encode_nbt(BinaryWriter&, const std::optional<std::string>& name, const ByteTag&);
-    AMULET_NBT_EXPORT void encode_nbt(BinaryWriter&, const std::optional<std::string>& name, const ShortTag&);
-    AMULET_NBT_EXPORT void encode_nbt(BinaryWriter&, const std::optional<std::string>& name, const IntTag&);
-    AMULET_NBT_EXPORT void encode_nbt(BinaryWriter&, const std::optional<std::string>& name, const LongTag&);
-    AMULET_NBT_EXPORT void encode_nbt(BinaryWriter&, const std::optional<std::string>& name, const FloatTag&);
-    AMULET_NBT_EXPORT void encode_nbt(BinaryWriter&, const std::optional<std::string>& name, const DoubleTag&);
-    AMULET_NBT_EXPORT void encode_nbt(BinaryWriter&, const std::optional<std::string>& name, const ByteArrayTag&);
-    AMULET_NBT_EXPORT void encode_nbt(BinaryWriter&, const std::optional<std::string>& name, const StringTag&);
-    AMULET_NBT_EXPORT void encode_nbt(BinaryWriter&, const std::optional<std::string>& name, const ListTag&);
-    AMULET_NBT_EXPORT void encode_nbt(BinaryWriter&, const std::optional<std::string>& name, const CompoundTag&);
-    AMULET_NBT_EXPORT void encode_nbt(BinaryWriter&, const std::optional<std::string>& name, const IntArrayTag&);
-    AMULET_NBT_EXPORT void encode_nbt(BinaryWriter&, const std::optional<std::string>& name, const LongArrayTag&);
-    AMULET_NBT_EXPORT void encode_nbt(BinaryWriter&, const std::string& name, const TagNode&);
-    AMULET_NBT_EXPORT void encode_nbt(BinaryWriter&, const NamedTag& tag);
+    AMULET_NBT_EXPORT void encode_nbt(BaseBinaryWriter&, const std::optional<std::string>& name, const ByteTag&);
+    AMULET_NBT_EXPORT void encode_nbt(BaseBinaryWriter&, const std::optional<std::string>& name, const ShortTag&);
+    AMULET_NBT_EXPORT void encode_nbt(BaseBinaryWriter&, const std::optional<std::string>& name, const IntTag&);
+    AMULET_NBT_EXPORT void encode_nbt(BaseBinaryWriter&, const std::optional<std::string>& name, const LongTag&);
+    AMULET_NBT_EXPORT void encode_nbt(BaseBinaryWriter&, const std::optional<std::string>& name, const FloatTag&);
+    AMULET_NBT_EXPORT void encode_nbt(BaseBinaryWriter&, const std::optional<std::string>& name, const DoubleTag&);
+    AMULET_NBT_EXPORT void encode_nbt(BaseBinaryWriter&, const std::optional<std::string>& name, const ByteArrayTag&);
+    AMULET_NBT_EXPORT void encode_nbt(BaseBinaryWriter&, const std::optional<std::string>& name, const StringTag&);
+    AMULET_NBT_EXPORT void encode_nbt(BaseBinaryWriter&, const std::optional<std::string>& name, const ListTag&);
+    AMULET_NBT_EXPORT void encode_nbt(BaseBinaryWriter&, const std::optional<std::string>& name, const CompoundTag&);
+    AMULET_NBT_EXPORT void encode_nbt(BaseBinaryWriter&, const std::optional<std::string>& name, const IntArrayTag&);
+    AMULET_NBT_EXPORT void encode_nbt(BaseBinaryWriter&, const std::optional<std::string>& name, const LongArrayTag&);
+    AMULET_NBT_EXPORT void encode_nbt(BaseBinaryWriter&, const std::string& name, const TagNode&);
+    AMULET_NBT_EXPORT void encode_nbt(BaseBinaryWriter&, const NamedTag& tag);
 
     AMULET_NBT_EXPORT std::string encode_nbt(const std::optional<std::string>& name, const ByteTag&, std::endian, Amulet::StringEncoder);
     AMULET_NBT_EXPORT std::string encode_nbt(const std::optional<std::string>& name, const ShortTag&, std::endian, Amulet::StringEncoder);

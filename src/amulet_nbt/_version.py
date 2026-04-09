78fee953193c4d25eee7785aae35b71daae2830a
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-04-09 11:16:11 +0100

    Update build tools for src layout

--- src/amulet_nbt/_version.py
+++ src/amulet_nbt/_version.py
@@ -52,7 +52,7 @@ def get_config() -> VersioneerConfig:
     cfg.style = "pep440"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = "amulet_nbt-"
-    cfg.versionfile_source = "amulet_nbt/_version.py"
+    cfg.versionfile_source = "src/amulet_nbt/_version.py"
     cfg.verbose = False
     return cfg
 

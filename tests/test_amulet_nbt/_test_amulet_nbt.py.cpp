77b4680d87da2fc2f750315e07be816ef854d5fd
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-04-16 15:20:18 +0100

    Split init modules

--- tests/test_amulet_nbt/_test_amulet_nbt.py.cpp
+++ tests/test_amulet_nbt/_test_amulet_nbt.py.cpp
@@ -5,15 +5,14 @@
 namespace py = pybind11;
 namespace pyext = Amulet::pybind11_extensions;
 
-void init_binary_nbt(py::module);
+void init_test_amulet_nbt(py::module);
 
-void init_module(py::module m){
+static void _init_test_amulet_nbt(py::module m){
     pyext::init_compiler_config(m);
     pyext::check_compatibility(py::module::import("amulet.nbt"), m);
-
-    init_binary_nbt(m);
+    init_test_amulet_nbt(m);
 }
 
 PYBIND11_MODULE(_test_amulet_nbt, m) {
-    m.def("init", &init_module, py::arg("m"));
+    m.def("init", &_init_test_amulet_nbt, py::arg("m"));
 }

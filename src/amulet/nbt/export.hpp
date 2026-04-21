2e20d38ef02ff5c5bff0ecb90acd63697ec783be
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-04-21 11:18:43 +0100

    Fix exception class

--- src/amulet/nbt/export.hpp
+++ src/amulet/nbt/export.hpp
@@ -8,14 +8,6 @@
             #define AMULET_NBT_EXPORT __declspec(dllimport)
         #endif
     #else
-        #define AMULET_NBT_EXPORT
-    #endif
-#endif
-
-#if !defined(AMULET_NBT_EXPORT_EXCEPTION)
-    #if defined(_LIBCPP_EXCEPTION)
-        #define AMULET_NBT_EXPORT_EXCEPTION __attribute__((visibility("default")))
-    #else
-        #define AMULET_NBT_EXPORT_EXCEPTION
+        #define AMULET_NBT_EXPORT __attribute__((visibility("default")))
     #endif
 #endif
